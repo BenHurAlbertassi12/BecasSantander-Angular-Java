346f0b6a0bcf124ce6fd941973c919d9e15820c4
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-09-06 13:35:11 -0300

    Do While

---
+++ EstruturasCondicionais/src/exemplo_do_wile/ExemploDoWhile.java
@@ -0,0 +1,12 @@
+package exemplo_do_wile;
+
+public class ExemploDoWhile {
+    public static void main(String[] args) {
+        int contador = 1;
+        do {
+            System.out.println(contador);
+            contador++;
+        } while (contador <= 5);
+    }
+}
+
