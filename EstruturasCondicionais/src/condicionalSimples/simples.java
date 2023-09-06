0d9597ffd70d4e41dbf396e40a0e5380a8bec397
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-09-06 11:39:24 -0300

    simples

--- EstruturasCondicionais/src/condicionalSimples/simples.java
+++ EstruturasCondicionais/src/condicionalSimples/simples.java
@@ -1,5 +1,14 @@
 package condicionalSimples;
 
-public class simples {
-    
+public class Simples {
+    public static void main(String[] args) {
+        double saldo = 25.0;
+        double valorSolicitado = 17.0;
+
+        if (valorSolicitado < saldo) {
+            saldo = saldo - valorSolicitado;
+        }
+
+        System.out.println(saldo);
+    }
 }
