a7651b99627476a00cfe3ed00db5fc10b97357d9
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-09-06 11:53:58 -0300

    Ternaria

---
+++ EstruturasCondicionais/src/condicao_ternaria/Ternaria.java
@@ -0,0 +1,11 @@
+package condicao_ternaria;
+
+public class Ternaria {
+    public static void main(String[] args) {
+        int nota = 8;
+        String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+
+            System.out.println(resultado);
+        
+    }
+}
