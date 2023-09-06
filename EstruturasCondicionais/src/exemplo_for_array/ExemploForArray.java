eceda47553a8b599676d54a56ff7c0a1eb59d68b
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-09-06 13:30:18 -0300

    For Array

---
+++ EstruturasCondicionais/src/exemplo_for_array/ExemploForArray.java
@@ -0,0 +1,12 @@
+package exemplo_for_array;
+
+public class ExemploForArray {
+    public static void main(String[] args) {
+        int[] numeros = {1, 2, 3, 4, 5};
+
+        // Usando um loop "for" para percorrer o array
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println(numeros[i]);
+        }
+    }
+}
