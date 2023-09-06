e22a4368ae2d9d218c9adaf5aad29f82a7f555a0
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-09-06 12:54:12 -0300

    for

---
+++ EstruturasCondicionais/src/exemplo_for/ExemploFor.java
@@ -0,0 +1,8 @@
+package exemplo_for;
+public class ExemploFor {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
