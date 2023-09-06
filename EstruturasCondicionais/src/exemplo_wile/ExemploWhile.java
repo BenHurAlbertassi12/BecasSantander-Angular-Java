28d472c1ef9146c811a6e4d9b702d5516abd0f34
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-09-06 13:34:24 -0300

    exemplo while

---
+++ EstruturasCondicionais/src/exemplo_wile/ExemploWhile.java
@@ -0,0 +1,11 @@
+package exemplo_wile;
+
+public class ExemploWhile {
+    public static void main(String[] args) {
+        int contador = 1;
+        while (contador <= 5) {
+            System.out.println(contador);
+            contador++;
+        }
+    }
+}
