2330df2f894a53e9140637e9d516ead3e4285b11
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-09-06 23:05:24 -0300

    .

--- Validação de Processo Seletivo/src/candidatura/ProcessoSeletivo.java
+++ Validação de Processo Seletivo/src/candidatura/ProcessoSeletivo.java
@@ -1,22 +1,105 @@
 package candidatura;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class ProcessoSeletivo {
-    static double salarioBase = 2000.00; // Declarando a variável salarioBase fora do método main
 
     public static void main(String[] args) {
-        analisarCandidato(1900.0);
-        analisarCandidato(2900.0);
-        analisarCandidato(2000.0);
- }
+        Scanner scanner = new Scanner(System.in);
+        List<Candidato> candidatos = new ArrayList<>();
 
-    // Definindo o método analisarCandidato com um parâmetro de entrada (salarioPretendido)
-    static void analisarCandidato(double salarioPretendido) {
-        if (salarioBase > salarioPretendido) {
-            System.out.println("Ligar para o candidato");
-        } else if (salarioBase == salarioPretendido) {
-            System.out.println("Aguardando resposta do candidato");
-        } else {
-            System.out.println("Ligar para o candidato com contra proposta");
+
+candidatos.add(new Candidato("Pedro Almeida", "pedro@email.com", "5678901234", "Desenvolvedor Full-Stack"));
+candidatos.add(new Candidato("Ana Pereira", "ana@email.com", "8765432109", "Especialista em Front-End"));
+candidatos.add(new Candidato("Carlos Rodrigues", "carlos@email.com", "2345678901", "Experiência em Python"));
+candidatos.add(new Candidato("Larissa Costa", "larissa@email.com", "7890123456", "Designer de UI/UX"));
+candidatos.add(new Candidato("Fernando Oliveira", "fernando@email.com", "3456789012", "Engenheiro de Software"));
+candidatos.add(new Candidato("Juliana Fernandes", "juliana@email.com", "9012345678", "Scrum Master"));
+candidatos.add(new Candidato("Ricardo Martins", "ricardo@email.com", "4321098765", "Analista de Dados"));
+candidatos.add(new Candidato("Mariana Silva", "mariana@email.com", "2109876543", "Desenvolvedora Mobile"));
+candidatos.add(new Candidato("Luiz Gonçalves", "luiz@email.com", "6543210987", "Arquiteto de Software"));
+candidatos.add(new Candidato("Camila Pereira", "camila@email.com", "1098765432", "Especialista em Segurança Cibernética"));
+ // Adicione mais candidatos conforme necessário
+
+        while (true) {
+            System.out.println("Selecione uma opção:");
+            System.out.println("1. Adicionar novo candidato");
+            System.out.println("2. Validar candidato");
+            System.out.println("3. Sair");
+
+            int opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    // Adicionar um novo candidato
+                    scanner.nextLine(); // Limpar o buffer
+                    System.out.println("Nome do candidato:");
+                    String nome = scanner.nextLine();
+                    System.out.println("Email do candidato:");
+                    String email = scanner.nextLine();
+                    System.out.println("Telefone do candidato:");
+                    String telefone = scanner.nextLine();
+                    System.out.println("Resumo das qualificações do candidato:");
+                    String qualificacoes = scanner.nextLine();
+                    Candidato novoCandidato = new Candidato(nome, email, telefone, qualificacoes);
+                    candidatos.add(novoCandidato);
+                    System.out.println("Candidato adicionado com sucesso!");
+                    break;
+                case 2:
+                    // Validar um candidato
+                    scanner.nextLine(); // Limpar o buffer
+                    System.out.println("Digite o nome do candidato que deseja validar:");
+                    String nomeValidar = scanner.nextLine();
+                    boolean encontrado = false;
+                    for (Candidato candidato : candidatos) {
+                        if (candidato.getNome().equalsIgnoreCase(nomeValidar)) {
+                            System.out.println("Candidato encontrado:");
+                            System.out.println(candidato);
+                            encontrado = true;
+                            break;
+                        }
+                    }
+                    if (!encontrado) {
+                        System.out.println("Candidato não encontrado.");
+                    }
+                    break;
+                case 3:
+                    // Sair do programa
+                    System.out.println("Saindo do programa. Até mais!");
+                    scanner.close();
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Opção inválida. Por favor, escolha uma opção válida.");
+            }
         }
+    }
+}
+
+class Candidato {
+    private String nome;
+    private String email;
+    private String telefone;
+    private String qualificacoes;
+
+    public Candidato(String nome, String email, String telefone, String qualificacoes) {
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+        this.qualificacoes = qualificacoes;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Nome: " + nome +
+                "\nEmail: " + email +
+                "\nTelefone: " + telefone +
+                "\nQualificações: " + qualificacoes;
     }
 }
